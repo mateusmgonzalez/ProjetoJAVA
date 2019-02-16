bf9279580f1e9ae6c79fe4ba3d77e1cf6a5c4934
Mateus Machado Gonzalez <mateusmgonzalez@gmail.com>
2019-02-16 00:49:47 -0200

    Criando operacao de instanciacao

--- src/main/java/com/treino/web/demo/DemoApplication.java
+++ src/main/java/com/treino/web/demo/DemoApplication.java
@@ -1,14 +1,33 @@
 package com.treino.web.demo;
 
+import com.treino.web.demo.domain.Categoria;
+import com.treino.web.demo.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		repository.saveAll(Arrays.asList(cat1,cat2));
+
+
+	}
 }
 
